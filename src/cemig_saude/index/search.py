c1cbed7e071f5511ff7b952284a4add4c68b2031
Gustavo Rauber <rauber@gmail.com>
2014-11-18 14:31:48 -0200

    uncommitted files

--- src/cemig_saude/index/search.py
+++ src/cemig_saude/index/search.py
@@ -34,3 +34,32 @@ def search_physicians(query="", n=10, distance=None, lat=None, lon=None):
             })
     
     return res['hits']['hits']
+
+def search_specialties(distance=None, lat=None, lon=None):
+    es = Elasticsearch()
+    res = es.search(index='physicians', doc_type="physician", 
+            body={  "query": {
+                        "match_all": {},                      
+                    },
+                    "facets": {
+                        "specialty": {
+                            "terms": {
+                                "field": "specialty",
+                                "all_terms": True
+                            }
+                        }
+                    },
+                    "size": n,
+                    "filter": {
+                         "geo_distance": {
+                             "distance": str(distance) + "km",
+                             "physician.location": {
+                                 "lat": lat,
+                                 "lon": lon
+                             }
+                         }
+                    }
+            })
+    
+    
+    return res['hits']['hits']
